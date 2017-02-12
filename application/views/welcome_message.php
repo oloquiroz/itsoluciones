ee5b65da4000d1abedff54bd6393407565fb6b31
octavio <oloquiroz@localhost.hm>
2017-02-12 00:04:07 -0600

    se agrega la inicializacion del proyecto itsoluciones

---
+++ application/views/welcome_message.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>ITsoluciones</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+</head>
+<body>
+
+<div class="container">
+	<div class="row">
+		<h1>hola mundo</h1>
+	</div>
+</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script> 
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
